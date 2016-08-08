ec73e6a02ea24eb31b2e5b4e09eeb7572e378f98
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2016-08-08 16:55:12 +0300

    Initial commit

---
+++ src/RunMe.java
@@ -0,0 +1,24 @@
+import aaa.bbb.ccc.Bean1;
+import aaa.bbb.ccc.Bean2;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 7/29/2016.
+ * Project: propertyHyphenName
+ * *******************************
+ */
+public class RunMe {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
+        Bean1 bean11 = (Bean1) ctx.getBean("b1");
+        Bean1 bean12 = (Bean1) ctx.getBean("b2");
+        Bean2 bean21 = (Bean2) ctx.getBean("b3");
+        Bean2 bean22 = (Bean2) ctx.getBean("b4");
+        System.out.println(bean11.toString());
+        System.out.println(bean12.toString());
+        System.out.println(bean21.toString());
+        System.out.println(bean22.toString());
+    }
+}
---
+++ src/aaa/bbb/ccc/Bean2.java
@@ -0,0 +1,50 @@
+package aaa.bbb.ccc;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 7/29/2016.
+ * Project: propertyHyphenName
+ * *******************************
+ */
+public class Bean2 {
+
+    String mySuperProp1;
+    String mySuperProp_2;
+    String mySuperProp3;
+
+
+
+    public String getMySuperProp1() {
+        return mySuperProp1;
+    }
+
+    public void setMySuperProp1(String mySuperProp1) {
+        this.mySuperProp1 = mySuperProp1;
+    }
+
+    public String getMySuperProp_2() {
+        return mySuperProp_2;
+    }
+
+    public void setMySuperProp_2(String mySuperProp_2) {
+        this.mySuperProp_2 = mySuperProp_2;
+    }
+
+    public String getMySuperProp3() {
+        return mySuperProp3;
+    }
+
+    public void setMySuperProp3(String mySuperProp3) {
+        this.mySuperProp3 = mySuperProp3;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bean1{");
+        sb.append("mySuperProp1='").append(mySuperProp1).append('\'');
+        sb.append(", mySuperProp_2='").append(mySuperProp_2).append('\'');
+        sb.append(", mySuperProp3='").append(mySuperProp3).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
